e6e31cb55ceec870ccccee16cdfcfb6c38a35239
Umesh Dhaked <umesh.dhaked@walmart.com>
2024-02-25 20:01:10 +0530

    added jwt auth and middleware for context params

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -1,5 +1,10 @@
 package models
 
-type TesingServer struct {
+type TesingRequest struct {
+	Message string `json:"message" binding:"required"`
+	Jwt     string `json:"jwt" binding:"required"`
+}
+
+type TestingResponse struct {
 	Message string `json:"message" binding:"required"`
 }
